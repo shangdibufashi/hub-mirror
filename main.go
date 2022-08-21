02b608b8cfacb4ae5c7d001dfd442e4076837fa1
leo <48305025+shangdibufashi@users.noreply.github.com>
2022-08-21 15:49:30 +0800

    Update main.go

--- main.go
+++ main.go
@@ -83,9 +83,9 @@ func main() {
 		// 如果为空,默认推送到 DockerHub 用户名 下
 		// 如果指定了值,则推动到指定的仓库下,用户名不一定与repository后缀相同
 		if *repository == "" {
-			target = *username + "/" + strings.ReplaceAll(source, "/", ".")
+			target = *username + "/" + strings.ReplaceAll(source, "/", "_")
 		} else {
-			target = *repository + "/" + strings.ReplaceAll(source, "/", ".")
+			target = *repository + "/" + strings.ReplaceAll(source, "/", "_")
 		}
 
 		wg.Add(1)
